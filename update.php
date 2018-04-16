65000df06f5341e2de4b98f81eac64a40f79bfc4
Jannik Vieten <me@exploide.net>
2018-04-16 14:23:52 +0200

    Some improvements
    * create tables in another order during db population to allow schema changes in the future
    
    * waste less space for lat/lon db fields. 5 decimal digits after the period are probably enough
    
    * made data parsing on update more resilent to input format changes
    
    * fixed querying the database based on the assumption that left < right and bottom < top
    
    * index relevant DB columns to speed up queries

--- update.php
+++ update.php
@@ -14,40 +14,43 @@ if ($mysqli->connect_errno) {
 }
 
 // prepare for updating database
-if (!($mysqli->query("CREATE TABLE IF NOT EXISTS oneway(
-                  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-                  wayId BIGINT UNSIGNED,
-                  fromNodeId BIGINT UNSIGNED,
-                  toNodeId BIGINT UNSIGNED,
-                  latitude DOUBLE(25,20),
-                  longitude DOUBLE(25,20)
-                )")))
-{
-  echo $mysqli->error;
-  exit(1);
-}
-
 if (!($mysqli->query("DROP TABLE IF EXISTS oneway_new"))) {
   echo $mysqli->error;
   exit(1);
 }
 
-if (!($mysqli->query("CREATE TABLE oneway_new LIKE oneway"))) {
+if (!($mysqli->query("CREATE TABLE IF NOT EXISTS oneway_new(
+                      id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                      wayId BIGINT UNSIGNED,
+                      fromNodeId BIGINT UNSIGNED,
+                      toNodeId BIGINT UNSIGNED,
+                      latitude DOUBLE(10,5),
+                      longitude DOUBLE(10,5),
+                      INDEX pos (latitude, longitude)
+                      )")))
+{
   echo $mysqli->error;
   exit(1);
 }
 
+if (!($mysqli->query("CREATE TABLE IF NOT EXISTS oneway LIKE oneway_new"))) {
+  echo $mysqli->error;
+  exit(1);
+}
+
+// prepare insert statement
 if (!($stmt = $mysqli->prepare("INSERT INTO oneway_new(wayId, fromNodeId, toNodeId, latitude, longitude) VALUES (?, ?, ?, ?, ?)")))
 {
   echo $mysqli->error;
   exit(1);
 }
 
-if (!($stmt->bind_param("iiidd", $wayID, $fromNodeId, $toNodeId, $latitude, $longitude))) {
+if (!($stmt->bind_param("iiidd", $wayId, $fromNodeId, $toNodeId, $latitude, $longitude))) {
   echo $stmt->error;
   exit(1);
 }
 
+// using transaction and commit is crucial for acceptable performance during db population
 if (!($mysqli->query("START TRANSACTION"))) {
   echo $mysqli->error;
   exit(1);
@@ -56,14 +59,26 @@ if (!($mysqli->query("START TRANSACTION"))) {
 // get new data from server
 $file = gzfile("https://missingroads.skobbler.net/dumps/OneWays/directionOfFlow_" . date("Ymd") . ".csv.gz");
 
+$headline = str_getcsv($file[0], ";");
+$pos_wayId = array_search("wayId", $headline);
+$pos_fromNodeId = array_search("fromNodeId", $headline);
+$pos_toNodeId = array_search("toNodeId", $headline);
+$pos_status = array_search("status", $headline);
+$pos_theGeom = array_search("theGeom", $headline);
+
+if ($pos_wayId === FALSE || $pos_fromNodeId === FALSE || $pos_toNodeId === FALSE || $pos_status === FALSE || $pos_theGeom === FALSE) {
+  echo "Error: Input data format changed in an unpredictable manner!";
+  exit(1);
+}
+
 // convert data into desired format and prepare db operations
 foreach ($file as $line) {
   $csv = str_getcsv($line, ";");
-  if (isset($csv[4]) && $csv[4] == "OPEN") {
-    $centroid = get_centroid(geoPHP::load($csv[6], 'wkt'));
-    $wayID = $csv[0];
-    $fromNodeId = $csv[1];
-    $toNodeId = $csv[2];
+  if (isset($csv[$pos_status]) && $csv[$pos_status] == "OPEN") {
+    $centroid = get_centroid(geoPHP::load($csv[$pos_theGeom], 'wkt'));
+    $wayId = $csv[$pos_wayId];
+    $fromNodeId = $csv[$pos_fromNodeId];
+    $toNodeId = $csv[$pos_toNodeId];
     $latitude = $centroid->getY();
     $longitude = $centroid->getX();
     if (!($stmt->execute())) {
@@ -85,7 +100,7 @@ if (!($mysqli->query("COMMIT"))) {
 }
 
 // make new data available and clean up old data
-if (!($mysqli->query("RENAME TABLE oneway TO oneway_old, oneway_new To oneway;"))) {
+if (!($mysqli->query("RENAME TABLE oneway TO oneway_old, oneway_new TO oneway;"))) {
   echo $mysqli->error;
   exit(1);
 }
